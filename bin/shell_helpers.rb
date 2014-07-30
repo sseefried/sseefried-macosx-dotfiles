194095a24bb0b1b352c2e5ab25d047e8569ae833
Sean Seefried <sean.seefried@gmail.com>
2014-07-30 10:52:47 +1000

    Added script to help GHC work well in Cabal Sandbox environments.

--- bin/shell_helpers.rb
+++ bin/shell_helpers.rb
@@ -1,35 +1,46 @@
 module ShellHelpers
 
   #
-  # Returns +true+ if the current directory is in a rails project.
+  # Returns truthy if the current directory is in a rails project.
   #
-  def in_rails_project?
-    try_in_parent_dirs { File.exist?('config/environment.rb') && File.exist?('Rakefile') }
+  def self.in_rails_project?
+    try_in_dir_or_parents { File.exist?('config/environment.rb') && File.exist?('Rakefile') }
   end
 
-  def has_rvmrc?
-    try_in_parent_dirs { File.exist?('.rvmrc') }
+  #
+  # Returns truthy if the there is a .rvmrc file present
+  #
+  def self.has_rvmrc?
+    try_in_dir_or_parents { File.exist?('.rvmrc') }
   end
 
-  module_function :in_rails_project?
-  module_function :has_rvmrc?
-
-  protected
-
   #
-  # Tries the given block in directory and parent directories until either the block returns +true+
-  # or +Dir.pwd == '/'+
+  # Takes a block and then tests the block in the current directory and all parent directories
+  # in order until the block evaluates to truthy or the the root directory '/' is reached.
+  # Return the first directory for which the block evaluates to truthy or +nil+.
   #
-  def self.try_in_parent_dirs
-    current = Dir.pwd
-    found = false
+  # e.g. Say you are searching for the existence of the .ruby-version file. It is in path
+  # /Users/billybob/project but you are currently in /Users/billybob/project/lib/tasks
+  #
+  # > ShellHelpers::try_in_dir_or_parents { File.exist? ".ruby-version" }
+  # "/Users/billbob/project"
+  #
+  # and
+  #
+  # > ShellHelpers::find_in_dir_or_parent { false }
+  # nil
+  #
+  #
+  def self.try_in_dir_or_parents
+    current = Dir.pwd # Save current directory
+    found = yield
     until found || Dir.pwd == '/'
-      found = yield
       Dir.chdir("..")
+      found = yield
     end
-    result = !(Dir.pwd == '/')
-    Dir.chdir(current)
-    result
+    dir = Dir.pwd if found
+    Dir.chdir(current) # Restore current directory
+    dir
   end
 
 end
